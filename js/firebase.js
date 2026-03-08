871965b5fa7e75d5d5f462c16cc3c7e45f297731
johnell <andrei_bublea@yahoo.com>
2026-03-08 12:21:37 +0200

    test

--- js/firebase.js
+++ js/firebase.js
@@ -1,49 +1,54 @@
 var config = {
-    apiKey: "AIzaSyDBTIa-gnm_tm_o9goIJiPx7KN3E4k6m_Q",
-    authDomain: "breakoutdb.firebaseapp.com",
-    databaseURL: "https://breakoutdb.firebaseio.com",
-    projectId: "breakoutdb",
-    storageBucket: "breakoutdb.appspot.com",
-    messagingSenderId: "706112964936"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "breakoutdb",
+  storageBucket: "breakoutdb.appspot.com",
+  messagingSenderId: "",
 };
 
 firebase.initializeApp(config);
 
 function writeUserData(userId, score, date) {
-    firebase.database().ref('Leaderboard/' + userId).set({
-      Score : score,
-      Date : date
+  firebase
+    .database()
+    .ref("Leaderboard/" + userId)
+    .set({
+      Score: score,
+      Date: date,
     });
-  } 
+}
 
 var provider = new firebase.auth.GoogleAuthProvider();
 var user;
 var token;
 
 function login() {
-  if (!user){
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      token = result.credential.accessToken;
-      // The signed-in user info.
-      user = result.user;
-      initAccount();
-      // ...
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-    });
+  if (!user) {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function (result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        token = result.credential.accessToken;
+        // The signed-in user info.
+        user = result.user;
+        initAccount();
+        // ...
+      })
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
   }
-
 }
 
-function retrieveData()
-{
+function retrieveData() {
   console.log("asd");
 }
